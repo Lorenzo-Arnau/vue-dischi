6f592f5f3afa90ab410ab97516c2d1765ab6971f
Lorenzo-Arnau <skyline03@hotmail.it>
2021-02-08 16:12:10 +0100

    add other 2 select

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ new Vue({
       option:'',
       listDisks:[],
       listGeneri:[],
+      listGroups:[],
+      listYears:[],
   },
   mounted() {
     const self = this;
@@ -11,24 +13,84 @@ new Vue({
       .then(function(response) {
         response.data.response.forEach((item, i) => {
         self.listDisks.push(item)
-        self.generateOptions();
+        var genere = 'genre';
+        var autore = 'author';
+        self.generateOptions(self.listDisks,genere,self.listGeneri);
+        self.generateOptions(self.listDisks,autore,self.listGroups);
+        self.generateYears();
         });
       });
   },
   methods:{
-      generateOptions : function(){
+      generateOptions : function(parentArray,value,destinationArray){
           const self = this;
-          self.listDisks.forEach((item, i) => {
-          if (!self.listGeneri.includes(item.genre)) {
-            self.listGeneri.push(item.genre)
-            console.log(self.listGeneri);
+          parentArray.forEach((item, i) => {
+          if (!destinationArray.includes(item[value])) {
+            destinationArray.push(item[value])
           }
         });
       },
-      filterList : function(element) {
+      generateYears : function(){
+          const self = this;
+          self.listDisks.forEach((item, i) => {
+          if (!self.listYears.includes(item.year)) {
+            let sweetYear = parseInt(item.year);
+             switch (true) {
+             case sweetYear > 1970 && sweetYear < 1980 && !self.listYears.includes('70s'):
+             self.listYears.push('70s');
+             break;
+             case sweetYear > 1980 && sweetYear < 1990 && !self.listYears.includes('80s'):
+             self.listYears.push('80s')
+             break;
+             case sweetYear > 1990 && sweetYear < 2000 && !self.listYears.includes('90s'):
+             self.listYears.push('90s')
+             break;
+             case sweetYear > 2000 && sweetYear < 2010 && !self.listYears.includes('2k'):
+             self.listYears.push('2k')
+             break;
+             case sweetYear > 2010 && sweetYear < 2020 && !self.listYears.includes('modern'):
+             self.listYears.push('modern')
+             break;
+           }
+          }
+        });
+      },
+      filterListByGenre : function(element) {
         if (element.genre == this.option) {
         return true;
-        }    
+        console.log(this.option);
+        }
+      },
+      filterListByAuthor : function(element) {
+        if (element.author == this.option) {
+        return true;
+        console.log(this.option);
+        }
+      },
+      filterListByYears : function(element) {
+         let sweetYear = parseInt(element.year);
+         var filterYears;
+         switch (true) {
+         case sweetYear > 1970 && sweetYear < 1980:
+         filterYears = '70s';
+         break;
+         case sweetYear > 1980 && sweetYear < 1990:
+          filterYears = '80s';
+         break;
+         case sweetYear > 1990 && sweetYear < 2000:
+          filterYears = '90s';
+         break;
+         case sweetYear > 2000 && sweetYear < 2010:
+          filterYears = '2k';
+         break;
+         case sweetYear > 2010 && sweetYear < 2020:
+          filterYears = 'modern';
+         break;
+       }
+        if (filterYears == this.option) {
+        return true;
+        console.log(this.option);
+        }
       },
   },
 });
